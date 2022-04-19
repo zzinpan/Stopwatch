f73989ff72543e80e36444285ba2677b3612af02
zzinpan <zzinpan@users.noreply.github.com>
2022-04-19 16:29:27 +0000

    Apply automatic changes

--- dist/iife/Stopwatch.js
+++ dist/iife/Stopwatch.js
@@ -116,48 +116,15 @@ var Stopwatch = (function () {
         return Degree;
     }());
 
-    /*! *****************************************************************************
-    Copyright (c) Microsoft Corporation.
-
-    Permission to use, copy, modify, and/or distribute this software for any
-    purpose with or without fee is hereby granted.
-
-    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-    PERFORMANCE OF THIS SOFTWARE.
-    ***************************************************************************** */
-    /* global Reflect, Promise */
-
-    var extendStatics = function(d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-
-    function __extends(d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    }
-
     /**
      * Data manager
      * @description
      *
      */
-    var DataManager = /** @class */ (function (_super) {
-        __extends(DataManager, _super);
+    // class DataManager extends Array<Data> implements Manager<Data, Stopwatch> {
+    var DataManager = /** @class */ (function () {
         function DataManager() {
-            return _super !== null && _super.apply(this, arguments) || this;
         }
-        // class DataManager implements Manager<Data, Stopwatch> {
         DataManager.prototype.get = function (stopwatch) {
             return this.find(function (data) {
                 return data.stopwatch === stopwatch;
@@ -178,7 +145,8 @@ var Stopwatch = (function () {
             return true;
         };
         return DataManager;
-    }(Array));
+    }());
+    Object.setPrototypeOf(DataManager.prototype, Array.prototype);
 
     /*** docs exclude
      * Stopwatch data
